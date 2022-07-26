a30e70b0e7c325b8718478bc62b96476cb381ed8
IronC <apehole@163.com>
2022-07-26 12:46:28 +0800

    bbb

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use rust_gpu_tools::Device;
+use fil_rustacuda::device::Device as Fil_Device;
 
 fn main() {
     let devices = Device::all();
     for d in devices {
         println!("{:?}", d.cuda_device().unwrap());
     }
+    println!("{:?}",Fil_Device::devices().unwrap());
 }
