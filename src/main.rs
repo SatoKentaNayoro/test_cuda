a4cd26142898ec6b041d74bba91d55c18bc72460
IronC <apehole@163.com>
2022-07-26 12:59:34 +0800

    bbb

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use rust_gpu_tools::Device;
-use fil_rustacuda::device::Device as Fil_Device;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::RwLock;
+// use rust_gpu_tools::Device;
+// use fil_rustacuda::device::Device as Fil_Device;
 
 fn main() {
-    let devices = Device::all();
-    for d in devices {
-        println!("{:?}", d.cuda_device().unwrap());
+    // let devices = Device::all();
+    // for d in devices {
+    //     println!("{:?}", d.cuda_device().unwrap());
+    // }
+    // println!("{:?}", Fil_Device::devices().unwrap());
+    let t_vec: RwLock<Vec<(u8, AtomicBool)>> = RwLock::new(Vec::new());
+    let mut guard = t_vec.write().unwrap();
+    guard.push((1, AtomicBool::new(false)));
+    drop(guard);
+    let mut guard_r = t_vec.write().unwrap();
+    if !guard_r[0].1.load(Ordering::SeqCst) {
+        guard_r[0].0 = 2
     }
-    println!("{:?}",Fil_Device::devices().unwrap());
+    drop(guard_r);
+    println!("{:?}", t_vec.read().unwrap()[0])
 }
