e1ec4409724fc5f5c545cb0a8d3296978765abbc
IronC <apehole@163.com>
2022-08-31 16:42:21 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::RwLock;
-// use rust_gpu_tools::Device;
-// use fil_rustacuda::device::Device as Fil_Device;
+use rand::RngCore;
+use snarkvm_algorithms::coinbase_puzzle::{Address, CoinbasePuzzle, EpochInfo};
+use snarkvm_algorithms::fft::DensePolynomial;
+use snarkvm_algorithms::polycommit::kzg10::{Powers, VerifierKey};
+use snarkvm_curves::bls12_377::Bls12_377;
+use snarkvm_utilities::rand::Uniform;
+use snarkvm_utilities::ToBytes;
 
 fn main() {
-    // let devices = Device::all();
-    // for d in devices {
-    //     println!("{:?}", d.cuda_device().unwrap());
-    // }
-    // println!("{:?}", Fil_Device::devices().unwrap());
-    let t_vec: RwLock<Vec<(u8, AtomicBool)>> = RwLock::new(Vec::new());
-    let mut guard = t_vec.write().unwrap();
-    guard.push((1, AtomicBool::new(false)));
-    drop(guard);
-    let mut guard_r = t_vec.write().unwrap();
-    if !guard_r[0].1.load(Ordering::SeqCst) {
-        guard_r[0].0 = 2;
-        guard_r[0].1.store(true, Ordering::SeqCst);
+    let max_degree = 1 << 15;
+    let mut rng = rand::thread_rng();
+
+    let srs = CoinbasePuzzle::<Bls12_377>::setup(max_degree, &mut rng);
+    for log_degree in 5..10 {
+        let degree = (1 << log_degree) - 1;
+        let product_degree = (1 << (log_degree + 1)) - 1;
+        let (pk, vk) = CoinbasePuzzle::trim(&srs, product_degree);
+        // println!("PK: {:?}", pk.vk.to_bytes_le().unwrap());
+        let epoch_info = EpochInfo { epoch_number: rng.next_u64() };
+        let epoch_challenge = CoinbasePuzzle::init_for_epoch(&epoch_info, degree);
+        let polynomial = DensePolynomial::from_coefficients_slice(&epoch_challenge.epoch_polynomial.coeffs);
+        assert_eq!(epoch_challenge.epoch_polynomial, polynomial);
+        for batch_size in 1..10 {
+            let solutions = (0..batch_size)
+                .map(|_| {
+                    let address = Address(<[u8; 32]>::rand(&mut rng));
+                    let x = address.to_bytes_le();
+                    assert_eq!(hex::encode(address.0), hex::encode(x));
+                    let nonce = u64::rand(&mut rng);
+                    CoinbasePuzzle::prove(&pk, &epoch_info, &epoch_challenge, &address, nonce)
+                })
+                .collect::<Vec<_>>();
+            let full_solution = CoinbasePuzzle::accumulate(&pk, &epoch_info, &epoch_challenge, &solutions);
+            assert!(CoinbasePuzzle::verify(&vk, &epoch_info, &epoch_challenge, &full_solution));
+            let bad_epoch_info = EpochInfo { epoch_number: rng.next_u64() };
+            let bad_epoch_challenge = CoinbasePuzzle::init_for_epoch(&bad_epoch_info, degree);
+            assert!(!CoinbasePuzzle::verify(&vk, &bad_epoch_info, &bad_epoch_challenge, &full_solution));
+        }
     }
-    drop(guard_r);
-    println!("{:?}", t_vec.read().unwrap()[0])
 }
